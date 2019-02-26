2722c33e8ce646aa878aaca9b80b7f688864a034
root <root@localhost.localdomain>
2019-02-26 22:19:48 +0000

    removed comments and imports

--- olxSearchTool/volantesic_find_all_brands_and_models.py
+++ olxSearchTool/volantesic_find_all_brands_and_models.py
@@ -2,20 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 
-from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 import pickle
-
 import olx_find_all_brands_and_models
-
-
-# TODO put all brands and models into datastruct for comparison
-# olx_find_all_brands_and_models.start_browser()
 
 def get_all_brands():
     brand_models_volantesic = {}
