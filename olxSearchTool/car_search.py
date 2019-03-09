695d55d0490f71057d11f145bfb2231574950907
root <root@localhost.localdomain>
2019-03-09 14:19:32 +0000

    all functionality to get car estimated prices based on volantesic implemented

--- olxSearchTool/car_search.py
+++ olxSearchTool/car_search.py
@@ -2,290 +2,56 @@ import requests
 from bs4 import BeautifulSoup
 import threading
 
-# import scapy
-# import re
-from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-
 import olx_find_all_brands_and_models
-import volantesic_find_all_brands_and_models
 import compare_models
+import standv_find_all_brands_and_models
+import volantesic_find_all_brands_and_models
+from car_links_struct import MaxScoreToLowForEvaluation
 
 
-def get_features_olx(soup):
-    price = soup.find_all('div', {"class": "price-label"})
-    price = ''.join(price[0].find_all(text=True))
-    price = price.replace("€", "")
-    price = price.replace(".", "")
-    price = price.strip()
-
-    price = int(price)
-    print(str(price))
-
-    used = soup.find('th', string="Condição").parent.a.text.strip()
-    if used.lower() == "usado":
-        used = True
-    else:
-        used = False
-    print(used)
-
-    km = soup.find('th', string="Quilómetros").parent.strong.text.strip().replace(".", "")
-    print(km)
-    km = int(km)
-
-    fuel_type = soup.find('th', string="Combustível").parent.a.text.strip()
-    print(fuel_type)
-
-    year = soup.find('th', string="Ano").parent.strong.text.strip()
-    print(year)
-    year = int(year)
-
-    model = soup.find('th', string="Modelo").parent.a.text.strip()
-    print(model)
-
-    name_feature = {"price": price, "used": used, "km": km, "fuel_type": fuel_type, "year": year, "model": model}
-    return name_feature
-
-
-def get_car_links_from_a_given_olx_page(url):
-    """
-    :param url: input url already has info on brand and model
-    :return: all cars for a given brand
-    """
-    # TODO this checks 1st page only, check all pages
-
-    # r = requests.get('https://www.olx.pt/carros-motos-e-barcos/carros/abarth/?search[filter_enum_modelo][0]=500&search[description]=1')
-    r = requests.get(url)
-    if r.status_code == requests.codes.ok:
-        # no olx todos os links na pagina que vai ate ao carro tem /anuncio/ , e' so' preciso procurar em todos esses links
-        matched_lines = [line for line in r.text.split('\"') if "/anuncio/" in line]
-        for cnt, line in enumerate(matched_lines):
-            new_line = line.split("html")
-            new_line = new_line[0] + "html"
-            matched_lines[cnt] = new_line
-
-        links_to_cars = list(set(matched_lines))
-        # print("number cars " + str(len(links_to_cars)))
-
-        # TODO use this to see if page changed from last time?
-        # TODO if a given brand has many pages of cars, check last page for number of cars? yes
-        # TODO default esta ordenado por mais recente no olx, o primeiro carro vai para a primeira pagina
-        # TODO after checking new car exists and identifying it check if it exists in persistent list
-        return links_to_cars
-
-
-def get_all_selecione_buttons_volantesic_for_olx(url):
-    """
-    # TODO this is what to do after getting all the parameters from olx, must get average from all prices in volantesic?
-    getting all buttons is useless for standvirtual
-    :param url: ex: https://volantesic.pt/marcas-carros/jaguar/daimler/2008/usado/
-    :return: all links attached to the selecione buttons
-    """
-    r = requests.get(url)
-    if r.status_code == requests.codes.ok:
-        soup = BeautifulSoup(r.text, "html.parser")
-        # print(soup)
-
-        links = soup.find_all("a", string="Selecione")
-        links = [link['href'] for link in links]
-        links = ["https://volantesic.pt" + link.replace("opcoes", "preco") for link in links]
-        print(links)
-        print(len(
-            links))  # aparecem todos os selecione ate' os escondidos (alguns estao escondidos pelo seleccao das opcoes
-
-        # for link in links:
-        link = links[0]
-
-
-def get_features(url, soup):
-    if "standvirtual" in url:
-        elements_dict = get_features_standvirtual(soup)
-    if "olx" in url:
-        elements_dict = get_features_olx(soup)
-
-    return elements_dict
-
-
-def get_dest_url_in_volantesic(features_dict):
-    brand = features_dict["brand"]  # set at the beggining manually in for loop (checks all brands)
-    model = features_dict['model']
-    year = features_dict['year']
-    # print(str(year))
-    used = features_dict['used']
-    if used:
-        used = "usado"
-    else:
-        used = "novo"
-
-    # closest year calculation
-    preview_url = 'https://volantesic.pt/' + brand + '/' + model
-    # print(preview_url)
-    preview_r = requests.get(preview_url)
-    soup = BeautifulSoup(preview_r.text, "html.parser")
-    links = soup.find_all('p', {"class": "carBlockTitle"})
-
-    prev_year_interval = 100
-    min_year = 100
-    for link in links:
-        test_year = link.strong.text
-        # print(year)
-        test_year = int(test_year)
-        year_interval = abs(year - test_year)
-        if year_interval < prev_year_interval:
-            min_year = test_year
-        if year_interval == 0:
-            break
-
-    year = min_year
-
-    dest_url = 'https://volantesic.pt/' + brand + '/' + model + '/' + str(year) + '/' + used
-    print(dest_url)
-    return requests.get(dest_url)
-
-
-def get_car_estimated_price_volantesic(browser, url):
-    browser.get(url)
-
-    # press buy private
-    browser.find_element_by_id("tabBuyPrivate").click()
-    wait = WebDriverWait(browser, 10)
-
-    # press the button to change km
-    element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[class=pricingMileageEdit]")))
-    element.click()
-
-    # input km number
-    element = wait.until(EC.element_to_be_clickable((By.ID, 'TextBoxKM')))
-    element.clear()
-    element.send_keys("100000")
-
-    # press button to change km
-    browser.find_element_by_css_selector("a[onclick*='VehiclePrice.SaveTexBoxKMPricing']").click()
-
-    # get value to compare to
-    element = wait.until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'svg[id*=drawSVG]')))  # wait for image to appear
-    soup = BeautifulSoup(browser.page_source, "html.parser")
-    base_value = soup.svg.find('tspan', string="Valor a Particular").parent
-    base_value = base_value.find_all('tspan', text=True)
-
-    # print(base_value)
-    for i in base_value:
-        if "€" in i.text:
-            base_value = i.text.replace("€", "").replace(" ", "").strip()
-    print(base_value)
-
-
-def get_features_standvirtual(url):
-    """
-    :param url: standvirtual's car url
-    :return: features_dict, key: feature name, val: feat value
-    """
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")
-
-    # standvirtual tem mto mais informacao, n e' preciso randomizar alguns campos na procura no bkk se for standvirtual
-    price = soup.find_all('span', {"class": "offer-price__number"})
-    price = ''.join(price[0].find_all(text=True))
-    price = price.replace("EUR", "")
-    price = price.strip()
-    price = price.replace(" ", "")
-
-    price = int(price)
-    print(str(price))
-
-    used = soup.find('span', string="Condição").parent.a.text.strip()
-    if used.lower() == "usados":
-        used = True
-    else:
-        used = False
-
-    print(used)
-
-    km = soup.find('span', string="Quilómetros").parent.div.text.strip().replace(" ", "")
-    km = km.replace("km", "")
-    print(km)
-    km = int(km)
-
-    fuel_type = soup.find('span', string="Combustível").parent.a.text.strip()
-    print(fuel_type)
-
-    year = soup.find('span', string="Ano de Registo").parent.div.text.strip()
-    print(year)
-    year = int(year)
-
-    model = soup.find('span', string="Modelo").parent.a.text.strip()
-    print(model)
-
-    # TODO
-    # Cilindrada
-    # Cor
-    # info de Caixa de mudancas
-    # traccao
-    # origem? - pais de origem
-
-    name_feature = {"price": price, "used": used, "km": km, "fuel_type": fuel_type, "year": year, "model": model}
-    return name_feature
-
-
-def get_car_accurate_page_volantesic(features):
-    # TODO
-    """
-    :param features: dictionary with key: parameter name, value: parameter ready for use by volantesic
-    :return: link to webpage from where you get the price from
-    """
-
-    # v_url = get_dest_url_in_volantesic(features)
-    v_url = "https://volantesic.pt/marcas-carros/alfa-romeo/giulietta/2011/usado/"
-
-
-def get_car_links_standv():
-    # TODO
-    pass
-
-
-def get_car_links_standv_to_compare_to_volantesic(comp_dic):
-    # TODO
-    pass
+def get_source_dest_imports(source, dest):
+    temp = []
+    if source == "standv":
+        temp += [standv_find_all_brands_and_models]
+    elif source == "olx":
+        temp += [olx_find_all_brands_and_models]
+    if dest == "volantesic":
+        temp += [volantesic_find_all_brands_and_models]
+    return temp
 
 
 def get_compare_dict(type_c):
-    compare_d = {}
-    if type_c == "olx_volantesic":
-        compare_d = compare_models.pickle_load_olx_volante()
-    elif type_c == "standv_volantesic":
-        compare_d = compare_models.pickle_load_standv_volante()
-    else:
-        print("type_c is wrong")
-    return compare_d
+    return compare_models.load_comparison_dic(type_c)
 
 
-type_comp = "standv"
-compare_dict = get_compare_dict(type_comp)
-# url = "https://volantesic.pt/abarth/500/2015/usado/comprar/preco/?ID=88279"
-# browser = olx_find_all_brands_and_models.start_browser()
-get_car_links_standv()  # these are the links from standv
-get_car_links_standv_to_compare_to_volantesic(compare_dict)  # only those being compared to volantesic
-standv_car_url = "https://www.standvirtual.com/anuncio/alfa-romeo-giulietta-1-6-jtdm-sport-nacional-ID8MqJJV.html" \
-                 "#1881796c1f;promoted "
-feature_dict = get_features_standvirtual(standv_car_url)
-get_car_accurate_page_volantesic(feature_dict)
+if __name__ == "__main__":
+    sources = ["standv"]  # , "olx"]
+    dests = ["volantesic"]
+    """
+    while True:
+        for source in sources:
+            for dest in dests:
+                """
 
-# get_car_estimated_price_volantesic(browser, url)
-# input("press key to exit")
-# browser.quit()
+    source = "standv"
+    dest = "volantesic"
+    type_comp = source + "_" + dest
+    source_import, dest_import = get_source_dest_imports(source, dest)
+    compare_dict = get_compare_dict(type_comp)
+    brand = "abarth"
+    model = "500"
+    lis = source_import.get_cars(brand, model)
+    for car_link_features in lis:
+        features = car_link_features.get_feats()
+        buttons = dest_import.get_buttons_from_features(brand, model, features, type_comp)
+        try:
+            max_score, max_score_buttons = dest_import.filter_buttons(buttons)
+            print("max_score " + str(max_score))
+            print()
+            print(str(len(max_score_buttons)))
+        except MaxScoreToLowForEvaluation:
+            print("no evaluation was possible, similarity score to low")
+        # dest_import.get_correct_price(lis)
 
-# TODO REFACTOR ESTE FICHEIRO E FICHEIRO OLX
-# TODO os clicks no que for preciso deve de estar no ficheiro associado ao
-#  site em que isso aconteces, por exemplo alguns aqui tem de estar no olx
-
-
-# TODO ignorar os que aparecem do standvirtual no olx e usar o proprio site do standvirtual para fazer os do standvirtual
-
-# TODO olx average
-
-
+# TODO the idea is to check standvirtual, then olx, all sources..., sequentialy and check if there is a new one and
+#  add that new one to our structure and then compare to volantesic, all destinations...,
--- olxSearchTool/volantesic_find_all_brands_and_models.py
+++ olxSearchTool/volantesic_find_all_brands_and_models.py
@@ -3,7 +3,15 @@ from bs4 import BeautifulSoup
 import sys
 
 import pickle
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
 import olx_find_all_brands_and_models
+import feats_mapper
+from car_links_struct import MaxScoreToLowForEvaluation
+
 
 def get_all_brands():
     brands_models_volantesic = {}
@@ -13,11 +21,11 @@ def get_all_brands():
     brand_names = soup.find_all('p', {"class": "brandSlideName"})
     for brand in brand_names:
         brand = brand.text.lower().replace(" ", "-")
-        #print(brand)
+        # print(brand)
         brands_models_volantesic[brand] = {}
-    #print
-    #print("number brands")
-    #print(len(brands_models_volantesic))
+    # print
+    # print("number brands")
+    # print(len(brands_models_volantesic))
     return brands_models_volantesic
 
 
@@ -51,6 +59,276 @@ def pickle_save(brands_models_volantesic):
     f_pickle.close()
 
 
+def get_dest_url(feats, brand, model):
+    year = feats.year
+    used = feats.used
+
+    # closest year calculation
+    preview_url = 'https://volantesic.pt/' + brand + '/' + model
+    # print(preview_url)
+    preview_r = requests.get(preview_url)
+    soup = BeautifulSoup(preview_r.text, "html.parser")
+    links = soup.find_all('p', {"class": "carBlockTitle"})
+
+    # TODO this is not ok, there should be a better way to get year, fix with scores fix
+    prev_year_interval = 100
+    min_year = 100
+    for link in links:
+        test_year = link.strong.text
+        # print(year)
+        test_year = int(test_year)
+        if test_year == year:
+            feats.is_exact_year()
+        year_interval = abs(year - test_year)
+        if year_interval < prev_year_interval:
+            min_year = test_year
+        if year_interval == 0:
+            break
+
+    year = min_year
+
+    dest_url = 'https://volantesic.pt/' + brand + '/' + model + '/' + str(year) + '/' + used
+    # print(dest_url)
+    return dest_url
+
+
+def get_clean_feats(feats, type_c):
+    """
+    :param type_c:
+    :param feats: src_feats
+    :return: feats list for volantesic
+    """
+    clean_feats = {}
+    feats_assoc = feats_mapper.pickle_load(type_c)
+    src_feats_dict = feats.get_feats_dict()
+    for feat_type in src_feats_dict:
+        feat_val = src_feats_dict[feat_type]
+        print(feat_type)
+        print(feat_val)
+        clean_feats[feat_type] = feats_assoc[feat_type][feat_val]
+
+    print(clean_feats)
+    return clean_feats
+
+
+def get_all_seleccione_buttons(url):
+    """
+    :param url: ex: https://volantesic.pt/marcas-carros/jaguar/daimler/2008/usado/
+    :return: all links attached to the selecione buttons
+    """
+
+    # this should be useful for olx maybe, but not used yet
+    r = requests.get(url)
+    if r.status_code == requests.codes.ok:
+        soup = BeautifulSoup(r.text, "html.parser")
+        # print(soup)
+
+        links = soup.find_all("a", string="Selecione")
+        links = [link['href'] for link in links]
+        links = ["https://volantesic.pt" + link.replace("opcoes", "preco") for link in links]
+        print(links)
+        print(len(
+            links))  # all selecciones appear ate' os escondidos (alguns estao escondidos pelo seleccao das opcoes
+
+
+def get_all_seleccione_text_and_button(url):
+    """
+    :param url: ex: https://volantesic.pt/marcas-carros/jaguar/daimler/2008/usado/
+    :return: all links attached to the selecione buttons
+    """
+    r = requests.get(url)
+    if r.status_code == requests.codes.ok:
+        soup = BeautifulSoup(r.text, "html.parser")
+        # print(soup)
+
+        seleccione_buttons = soup.find_all("a", string="Selecione")
+        seleccione_text_association = {}
+        for seleccione in seleccione_buttons:
+            version = seleccione.parent.parent.parent.find("div", {"class": "versionsListControls"}).p.text
+            features_text = seleccione.parent.p.text
+            seleccione_text_association[seleccione] = [features_text, version]
+            # print(seleccione_text_association)
+        return seleccione_text_association
+
+
+def get_buttons_from_features(brand, model, feats, type_c):
+    """
+    must give a score based on how precise the search is and show that score the score or similarity score is how
+    much the car from the source is similar to the destination, the higher the better
+    :param type_c:
+    :param feats: CarFeatures object
+    :return: returns list of all buttons associated with given feats and their score
+    """
+
+    buttons = {}  # key: button, val: score
+    url = get_dest_url(feats, brand, model)
+    r = requests.get(url)
+    t_year = -1
+    if r.status_code == requests.codes.ok:
+        soup = BeautifulSoup(r.text, "html.parser")
+        t_year = soup.find('span', {"itemprop": "releaseDate"}).text
+        print(t_year)
+    else:
+        print("error in request")
+
+    t_year = int(t_year)
+    sel_buttons = get_all_seleccione_text_and_button(url)
+    caixa_mudancas, traction, segmento, fuel_type = get_clean_feats(feats, type_c)
+    cv = int(feats.cv)
+    str_cv = str(cv) + "cv"
+    year = int(feats.year)
+    doors = str(feats.doors) + "p"
+    version = feats.version
+
+    for sel in sel_buttons:
+        select = sel_buttons[sel]
+        score = 0
+        text = select[0]
+        t_version = select[1].strip().lower()
+
+        text = text.strip().lower().split(" ")
+        t_segmento = text[0]
+        t_cv = text[1]
+        int_t_cv = t_cv.strip().lower().replace("cv", "")
+        int_t_cv = int(int_t_cv)
+        t_doors = text[2]
+        t_fuel_type = text[3]
+        t_traction = text[4]
+        t_caixa_mudancas = text[5]
+
+        # TODO fix how the scores work later
+        if caixa_mudancas in t_caixa_mudancas:
+            score += 5
+        if traction == t_traction:
+            score += 5
+        if segmento == t_segmento:
+            score += 5
+        if str_cv == t_cv:
+            score += 10
+        elif int_t_cv - 5 < cv < int_t_cv + 5:
+            score += 5
+        elif int_t_cv - 10 < cv < int_t_cv + 10:
+            score += 1
+        if doors == t_doors:
+            score += 5
+        if fuel_type == t_fuel_type:
+            score += 5
+        if version != "":
+            if version == t_version:
+                score += 30
+            elif version in t_version:
+                score += 15
+            else:
+                version_tokens = version.split(" ")
+                for token in version_tokens:
+                    if token in t_version:
+                        score += 1
+        if t_year == -1:
+            print("t_year=-1 error")
+            pass
+        elif year == t_year:
+            score += 5
+        elif year - 2 < t_year < year + 2:
+            score += 1
+
+        buttons[sel] = score
+    return buttons
+
+
+def filter_buttons(buttons):
+    """
+    :param buttons: input buttons, alongside scores key: button, val: score
+    :return: max_score, list with all buttons with max_score as key
+    """
+    max_score = 0
+    max_score_buttons = []
+    for i in buttons:
+        if buttons[i] > max_score:
+            max_score = buttons[i]
+    # TODO fix max_score minimum value once more info is collected on the scores and how to make them better
+    if max_score < 30:
+        raise MaxScoreToLowForEvaluation
+    for i in buttons:
+        if buttons[i] == max_score:
+            max_score_buttons += [i]
+
+    return max_score, max_score_buttons
+
+
+def get_car_estimated_price_volantesic(browser, car):
+    """
+
+    :param browser:
+    :param car:
+    :return: returns base value for a car
+    """
+    km = car.km
+    browser.get(car.link)
+
+    # press buy private
+    browser.find_element_by_id("tabBuyPrivate").click()
+    wait = WebDriverWait(browser, 10)
+
+    # press the button to change km
+    element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[class=pricingMileageEdit]")))
+    element.click()
+
+    # input km number
+    element = wait.until(EC.element_to_be_clickable((By.ID, 'TextBoxKM')))
+    element.clear()
+    element.send_keys(km)
+
+    # press button to change km
+    browser.find_element_by_css_selector("a[onclick*='VehiclePrice.SaveTexBoxKMPricing']").click()
+
+    # get value to compare to
+    element = wait.until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'svg[id*=drawSVG]')))  # wait for image to appear
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    base_value = soup.svg.find('tspan', string="Valor a Particular").parent
+    base_value = base_value.find_all('tspan', text=True)
+
+    # print(base_value)
+    for i in base_value:
+        if "€" in i.text:
+            base_value = i.text.replace("€", "").replace(" ", "").strip()
+    print(base_value)
+    return base_value
+
+
+def get_correct_estimate_price(car, type_c):
+    # this is to be used in import car_search.py
+    """
+    uses get_car_estimated_price_volantesic to get correct prices
+    :param type_c:
+    :param car: output of get_cars from source
+    :return: correct price is picked after inspecting
+    all prices from all given buttons and picks the lowest, to get worst possible evaluation and see if worst
+    possible is good, or returns None if there was no value worth returning (because the cars are not similar enough)
+    """
+    element = next(iter(car))
+    brand = element.get_brand()
+    model = element.get_model()
+    buttons = get_buttons_from_features(brand, model, element, type_c)
+    buttons_links = []
+    try:
+        max_score, max_score_buttons = filter_buttons(buttons)
+    except MaxScoreToLowForEvaluation:
+        print("no evaluation was possible, similarity score to low")
+        return None
+    for but in max_score_buttons:
+        url = but['href']
+        print(url)
+
+
+def get_all_cars_estimate_price(cars, type_c):
+    pass
+
+
+def build_estimate_struct():
+    pass
+
+
 def pickle_load():
     return olx_find_all_brands_and_models.load_brands_and_models("brands_and_models_volantesic")
 
@@ -79,3 +357,5 @@ if __name__ == "__main__":
         brands_models_volantesic = pickle_load()
 
     print(brands_models_volantesic)
+    print()
+
